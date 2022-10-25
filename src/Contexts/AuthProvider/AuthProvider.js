226199aeb5c1f4392331ea8c12912674a2dea547
almahfuz <almahfuz0327@gmail.com>
2022-10-25 18:19:25 +0600

    added google sign in

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,14 +1,31 @@
-import React, { createContext } from 'react'
-import { getAuth } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react'
+import { getAuth, onAuthStateChanged, signInWithPopup } from "firebase/auth";
 import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext();
 const auth = getAuth(app)
 
 export default function AuthProvider({ children }) {
+    // hooks
+    const [user, setUser] = useState(false)
+    console.log(user);
+    // functions
+    const googleSignIn = (provider) => {
+        return signInWithPopup(auth, provider);
+    }
 
-    const user = { name: 'abdullah' }
-    const authInfo = { user }
+    // onAuthStateChanged
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            if (currentUser || currentUser === null) {
+                setUser(true);
+            }
+        })
+
+        return () => unsubscribe();
+    }, [])
+
+    const authInfo = { user, googleSignIn }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,10 +1,25 @@
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { FaGoogle, FaGithub } from 'react-icons/fa';
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
+import { GoogleAuthProvider } from 'firebase/auth';
+
+const googleProvider = new GoogleAuthProvider()
 
 export default function Login() {
+    // from Auth context
+    const { googleSignIn } = useContext(AuthContext)
+
+    // functions
+    const handleGoogle = () => {
+        googleSignIn(googleProvider)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(err => { console.error(err) })
+    }
     return (
         <div>
             <div className=' mt-16 flex justify-center '>
@@ -21,7 +36,7 @@ export default function Login() {
                         <button className='border w-full bg-green-500 p-2 rounded mt-8 drop-shadow font-bold text-slate-700 hover:bg-green-400'>Sign In</button>
                         <small className='mt-2'>Don't have an account? <span className='text-red-400 hover:drop-shadow-lg hover:text-red-500'> <Link to='/register'> Register Here </Link> </span></small>
 
-                        <button className='border w-full p-2 mt-8 rounded hover:shadow-md shadow-green-400 drop-shadow text-slate-400 font-bold '>
+                        <button onClick={handleGoogle} className='border w-full p-2 mt-8 rounded hover:shadow-md shadow-green-400 drop-shadow text-slate-400 font-bold '>
                             <span className=' flex justify-center mb-1 text-red-400'> <FaGoogle /></span>  Continue with Google</button>
                         <button className='border w-full p-2 mt-4 rounded hover:shadow-md shadow-green-400 drop-shadow text-slate-400 font-bold'>
                             <span className=' flex justify-center mb-1 text-red-400'> <FaGithub /></span>
